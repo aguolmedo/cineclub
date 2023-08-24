cc2d06fe1e5860a56bb91d2b0ada66b603c4c792
aguolmedo <aguolmedofernandez@gmail.com>
2023-08-24 20:50:04 -0300

    fix

--- dist/services/types/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Types = void 0;
-exports.Types = {
-    AuthService: Symbol("IAuthService"),
-    HealthcheckService: Symbol("IHealthcheckService"),
-};
-exports.default = exports.Types;
-//# sourceMappingURL=types.js.map
